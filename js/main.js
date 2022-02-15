dac1b822507ddc0f90a58f76de49f13d7cf9fd23
rageCode153 <ChrisKonstantopoulos2002@gmail.com>
2022-02-15 02:04:38 +0200

    Added Game

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function reload_page() {
+    location.reload();
+}
+
+$(function () {
+    console.log(
+        "Created by Chris Konstantopoulos\nSocial Pages:\nGitHub: www.github.com/xriskon\nTwitter: www.twitter.com/xriskon\nInstagram: www.instagram.com/xriskon_\n   \\\n   (o>\n\\_//)\n _/_)\n _|_"
+    );
+});
