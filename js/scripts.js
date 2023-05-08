d8e4575cc793ef6cea48e0ef9fde358655843e8d
rageCode153 <ChrisKonstantopoulos2002@gmail.com>
2023-05-08 18:47:34 +0300

    Color Palette Change + Minor Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 const navbar = document.getElementById("navbar");
+const hero = document.getElementById("hero");
+
 window.onscroll = () => {
     if (window.scrollY > 100) {
         navbar.classList.add("nav-active");
     } else {
         navbar.classList.remove("nav-active");
+    }
+    if (window.scrollY > 210) {
+        hero.classList.add("hero-off");
+    } else {
+        hero.classList.remove("hero-off");
     }
 };
