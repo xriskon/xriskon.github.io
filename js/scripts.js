1f62bd3cace32ef0ee759ec908b9952570b87d02
rageCode153 <ChrisKonstantopoulos2002@gmail.com>
2023-05-23 18:57:56 +0300

    Rearrangement and Animations

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 const navbar = document.getElementById("navbar");
 const navList = document.getElementById("nav-list");
 const navToggle = document.getElementById("mobile-nav-toggle");
+const navLinks = document.querySelectorAll(".nav-list li");
 const hero = document.getElementById("hero");
+const html = document.querySelector("html");
+
+const hiddenElements = document.querySelectorAll(".hidden");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
 
 window.onscroll = () => {
     if (window.scrollY > 100) {
@@ -16,13 +30,17 @@ window.onscroll = () => {
     }
 };
 
-navToggle.addEventListener("click", () => {
-    const visibility = navList.getAttribute("data-visible");
-    if (visibility === "false") {
-        navList.setAttribute("data-visible", true);
-        navToggle.setAttribute("aria-expanded", true);
-    } else if (visibility === "true") {
-        navList.setAttribute("data-visible", false);
-        navToggle.setAttribute("aria-expanded", false);
-    }
+[navToggle, navList].forEach((item) => {
+    item.addEventListener("click", () => {
+        const visibility = navList.getAttribute("data-visible");
+        if (visibility === "false") {
+            navList.setAttribute("data-visible", true);
+            navToggle.setAttribute("aria-expanded", true);
+            html.classList.add("html-locked");
+        } else if (visibility === "true") {
+            navList.setAttribute("data-visible", false);
+            navToggle.setAttribute("aria-expanded", false);
+            html.classList.remove("html-locked");
+        }
+    });
 });
