abc6868ea15ce688152e955190f786587dbd1041
Chris Konstantopoulos <ChrisKonstantopoulos2002@gmail.com>
2023-10-10 19:40:06 +0300

    Slideshow

--- js/scripts.js
+++ js/scripts.js
@@ -69,3 +69,33 @@ function checkScroll() {
 
 // Run it initially
 handleDeviceChange(smallDevice);
+
+const wrapper = document.querySelector(".slideshow__wrapper");
+const slideWidth = document.querySelector(".info__slideshow").clientWidth;
+console.log(slideWidth);
+
+let pressed = false;
+let startX = 0;
+
+wrapper.addEventListener("mousedown", function (e) {
+	pressed = true;
+	startX = e.clientX;
+	this.style.cursor = "grabbing";
+});
+
+wrapper.addEventListener("mouseleave", function (e) {
+	pressed = false;
+});
+
+window.addEventListener("mouseup", function (e) {
+	pressed = false;
+	wrapper.style.cursor = "grab";
+});
+
+wrapper.addEventListener("mousemove", function (e) {
+	if (!pressed) {
+		return;
+	}
+
+	this.scrollLeft += startX - e.clientX;
+});
